d93f941da84198001460c875adba56bf0b6f963c
hwing777 <hwing777@gmail.com>
2019-09-30 19:58:34 -0700

    Add firebase to app

--- uji/src/App.js
+++ uji/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase.js'
 
 function App() {
   return (
---
+++ uji/src/firebase.js
@@ -0,0 +1,14 @@
+// src/firebase.js
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyCoR9TffQjePVhItJfaTut-3yL2T17SOSM",
+    authDomain: "uji-data.firebaseapp.com",
+    databaseURL: "https://uji-data.firebaseio.com",
+    projectId: "uji-data",
+    storageBucket: "uji-data.appspot.com",
+    messagingSenderId: "1010678289713",
+    appId: "1:1010678289713:web:12eff6d1fca0612292cc10",
+    measurementId: "G-XHM7QBL4ZR"
+};
+firebase.initializeApp(config);
+export default firebase;
