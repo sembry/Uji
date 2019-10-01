9664a7213ecc017f5a433c3c1e03f7737441e4c5
Sarah Embry <skembry@hotmail.com>
2019-10-01 12:46:49 -0700

    Add basic files for Post and Feed components

---
+++ uji/src/Components/FeedComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    
+  }
+}
+export default Feed;
---
+++ uji/src/Components/PostComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PostComponent.css'
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className = "Post">
+      </div>
+    );
+  }
+}
+export default Post;
