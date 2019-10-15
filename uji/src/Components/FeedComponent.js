5eb4c8fa0689a70c295ffb10cfc5f6515c99c79b
sembry <skembry@hotmail.com>
2019-10-15 09:27:37 -0700

    start adding posts to feed component state

--- uji/src/Components/FeedComponent.js
+++ uji/src/Components/FeedComponent.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Post from './PostComponent';
+import firebase from '../firebase.js';
 
 class Feed extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {posts: []};
   }
+
+// the idea here is to add the posts from the database to the state here
+
+  // componentWillMount() {
+  //   var database = firebase.database();
+  //   database.once('value', function(snapshot) {
+  //     snapshot.forEach(function(childSnapshot) {
+  //       var childKey = childSnapshot.key;
+  //       var childData = childSnapshot.val();
+  //   // ...
+  //     });
+  //   });
+  // }
+
+
+
   render() {
     return (
       <div>
