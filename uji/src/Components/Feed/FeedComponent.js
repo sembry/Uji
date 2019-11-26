4a9c8c0eed95f380f0c0c296c881127770659cfd
sembry <skembry@hotmail.com>
2019-11-25 19:24:31 -0800

    adjust text width

--- uji/src/Components/Feed/FeedComponent.js
+++ uji/src/Components/Feed/FeedComponent.js
@@ -8,14 +8,12 @@ import "animate.css/animate.min.css";
 // https://www.npmjs.com/package/react-animate-on-scroll
 import ScrollAnimation from 'react-animate-on-scroll';
 
-
 class Feed extends React.Component {
   constructor(props) {
     super(props);
     this.state =  {db: null,
       loading: true};
     }
-
     // fetches data asynchronously from the posts database
     componentDidMount = () => {
       const posts = firebase.database().ref('posts');
